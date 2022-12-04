0517906387aaaf08f2224bb34e4892b73534518a
Raja Biswas2 <rbiswas2@lenovo.com>
2022-12-04 22:28:03 +0530

    Add actuator info change

--- src/main/java/com/springboot/actuatorinfo/ActuatorInfoApplication.java
+++ src/main/java/com/springboot/actuatorinfo/ActuatorInfoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActuatorInfoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActuatorInfoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ActuatorInfoApplication.class, args);
+  }
 }
