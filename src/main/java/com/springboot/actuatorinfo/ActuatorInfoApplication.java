e0d0b14255f2da28f2b02582586c9453b3b806f7
Raja Biswas2 <rbiswas2@lenovo.com>
2022-12-04 21:46:01 +0530

    Add skeleton project code

---
+++ src/main/java/com/springboot/actuatorinfo/ActuatorInfoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.actuatorinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/actuatorinfo/ActuatorInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.actuatorinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
